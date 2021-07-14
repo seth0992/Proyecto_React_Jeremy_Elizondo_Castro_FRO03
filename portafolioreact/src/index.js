09deaab02d326eb1c73700e93d76ebbdbf35d487
Jeremy Elizondo <braseth.16@outlook.com>
2021-07-14 17:41:24 -0600

    Build 2

--- portafolioreact/src/index.js
+++ portafolioreact/src/index.js
@@ -5,45 +5,6 @@ import MenuBar from "./Componentes/MenuBar/MenuBar";
 // import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
-// Posibles componentes del proyecto
-// Componente para el menu
-// function navBar() {
-//   return <div>Menu</div>;
-// }
-
-// Componente para el hero banner del portafolio
-// function header() {
-//   return <div>Sección para el hero banner</div>;
-// }
-// Componente para la información personal
-// function perfil() {
-//   return <div>Sección para la información personal</div>;
-// }
-// Componente para los servicios del portafolio
-// function servicios() {
-//   return <div>Sección para los servicios</div>;
-// }
-// Componente para los trabajos realizados
-// function trabajos() {
-//   return <div>Sección para la información personal</div>;
-// }
-// Componente para el formulario de contacto
-// function contacto() {
-//   return <div>Sección para el formulario de contacto</div>;
-// }
-// Componente para el footer
-function footer() {
-  return <div>Sección para el footer</div>;
-}
-// Componente para el blog de la pagina
-function paginaBlog() {
-  return <div>Sección general para el blog</div>;
-}
-// Componente para el la entrada del blog
-function blogEntrada() {
-  return <div>Entrada del blog</div>;
-}
-
 ReactDOM.render(
   // <React.StrictMode>
   [<MenuBar />],
